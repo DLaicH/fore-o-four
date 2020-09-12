559db97ba721c5c6c23eaee0f935b45d070a2d8e
David L. Herring <davidlherring@gmail.com>
2020-09-12 09:40:30 -0400

    tweaks

--- src/Ball.svelte
+++ src/Ball.svelte
@@ -13,40 +13,6 @@
 	$: counterTilt = 360 - $tilt;
 	let counterRotation = 0;
 	$: counterRotation = 360 - $rotation;
-
-	const unsub = ballZ.subscribe($z => console.log($z));
-
-	// $: if (hit) {
-	// 	doHit();
-	// }
-
-	// function doHit() {
-
-
-	// 	console.log('doing hit');
-	// 	el.animate([
-	// 		{transform: 'translate3d(0, 0, 0)'},
-	// 		{transform: 'translate3d(-16rem, 16rem, 0)'}
-	// 	], {
-	// 		duration: 2000,
-	// 		//composite: 'accumulate'
-	// 	})
-	// 	.onfinish = () => {
-	// 		el.style.transform = 'translate3d(-16rem, 16rem, 0)';
-	// 		ballX.set(-16);
-	// 		ballY.set(16);
-	// 	};
-
-	// 	elZ.animate([
-	// 		{transform: 'translateZ(0)', easing: 'cubic-bezier(0.0, 0.0, 0.58, 1.0)'},
-	// 		{transform: 'translateZ(12rem)', easing: 'cubic-bezier(0.42, 0.0, 1.0, 1.0)'},
-	// 		{transform: 'translateZ(0)'}
-	// 	], {
-	// 		duration: 2000,
-	// 		//composite: 'accumulate'
-	// 	});
-	// }
-
 </script>
 
 <style>
@@ -60,20 +26,25 @@
 		left: 50vw; */
 		top: -0.5rem;
 		left: -0.5rem;
-		width: 1rem;
-		height: 1rem;
-		background-color: rgba(0, 0, 0, 0.5);
-		border-radius: 50%;
+		
 		transform: translate3d(0, 0, 0);
 		perspective: 50rem;
 		transform-style: preserve-3d;
+	}
+
+	.ball-shadow {
+		width: 1rem;
+		height: 1rem;
+		background-color: rgba(0, 0, 0, 0.5);
+		border-radius: 0.5rem;
+		position: absolute;
 	}
 
 	.ball {
 		width: 1rem;
 		height: 1rem;
 		background-color: #fff;
-		border-radius: 50%;
+		border-radius: 0.5rem;
 		position: absolute;
 		/* transform: translate3d(0, 0, 0.5rem) rotate3d(1, 0, 0, 0); */
 		perspective: 50rem;
@@ -87,5 +58,7 @@
 			style="transform: translate3d(0, 0, 0.5rem) rotateZ({counterRotation}deg) rotate3d(1, 0, 0, {counterTilt}deg)"
 		></div>
 	</div>
+
+	<div class="ball-shadow"></div>
 </div>
 
